e66860ca6db4337ebe48616c79b18f0340d0b7d0
Askansz <122436444+Askansz@users.noreply.github.com>
2024-01-29 12:15:12 +0100

    Delete api directory

--- api/sendMessage.js
+++
@@ -1,9 +0,0 @@
-// api/sendMessage.js
-module.exports = async (req, res) => {
-  const { url } = req.query;
-  const { message } = req.body;
-
-  // Logic to store the message (you can use a database or any storage mechanism)
-
-  res.status(200).json({ success: true });
-};
